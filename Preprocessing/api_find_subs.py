9c4e216837d2489b7b5e5e9ee26e763858123ff3
jvschlierf <67417254+jvschlierf@users.noreply.github.com>
2022-05-23 23:34:30 +0200

    aggregation + oop

--- Preprocessing/api_find_subs.py
+++ Preprocessing/api_find_subs.py
@@ -2,7 +2,8 @@
 from psaw import PushshiftAPI
 import time
 import datetime as dt 
-import pandas as pd 
+import pandas as pd
+import pickle 
 
 api = PushshiftAPI()
 start_epoch = int(dt.datetime(2021, 2, 1).timestamp())
@@ -10,25 +11,25 @@ start_epoch = int(dt.datetime(2021, 2, 1).timestamp())
 
 subreddits = ['DebateVaccines', 'CovidVaccinated', 'Vaccine', 'Coronavirus', 'LockdownSkepticism', 'HermanCainAward', 'NoNewNormal']
 
-def get_crossposts(start_epoch, subreddits): #Need to define varying time / depth of crosspost to look at
+def get_crossposts(start_epoch, subreddits, outfile): #Need to define varying time / depth of crosspost to look at
     
     api = PushshiftAPI()
     results = []
-    df = pd.DataFrame(columns= ['subreddit', 'subreddit_id',  
+    df = pd.DataFrame(columns= ['subreddit', 'subreddit_id', 'subreddit_subscibers' 
                 'crosspost_parent', 'crosspost_parent_list'])
 
     for i in subreddits:
         gen = api.search_submissions(
             after = start_epoch,
             subreddit = i,
-            filter=['subreddit', 'subreddit_id',  
-                'crosspost_parent', 'crosspost_parent_list'],
+            filter=['subreddit', 'subreddit_id', 'subreddit_subscribers',
+                'crosspost_parent', 'crosspost_parent_list', 'author'],
             limit = 600)
 
-        results = (list(gen))
+        results = list(gen)
         print(f'subreddit: {i}, number of results: {len(results)}')
         temp = pd.DataFrame([thing.d_ for thing in results])
-        time.sleep(2)
+        time.sleep(4)
 
         df = pd.concat([df, temp])
 
@@ -39,16 +40,24 @@ def get_crossposts(start_epoch, subreddits): #Need to define varying time / dept
         t = dict(df2.loc[i,'crosspost_parent_list'][0])
         df2.loc[i,'crosspost_from'] = t['subreddit']
         df2.loc[i,'crosspost_from_id'] = t['subreddit_id']
-        # df2.loc[i,'crosspost_from_subs'] = t['subreddit_subscribers'] # find way to get average of subscribers so groupby works
+        df2.loc[i,'crosspost_from_subs'] = t['subreddit_subscribers'] 
 
-    df3 = pd.DataFrame(df2.groupby(['subreddit','subreddit_id','crosspost_from', 'crosspost_from_id']).size().reset_index())
-    df3.rename(columns = {0:'Count'}, inplace = True)
-    # df3['crosspost_from_subs'] = df3['crosspost_from_subs'].astype(int)
+    df2.to_pickle(f'../../Files/{outfile}.pickle')
+    return df2
     
+
+def aggregate(df):
+    df3 = df.groupby(['subreddit','subreddit_id','crosspost_from', 'crosspost_from_id']).agg({'subreddit_subscribers': 'mean', 'crosspost_from_subs': 'mean' , 'author' : 'count'}).reset_index()
+    df3.rename(columns = {0:'Count'}, inplace = True)
+    df3['crosspost_from_subs'] = df3['crosspost_from_subs'].astype(int)
+
     return df3
 
 
 
-df3 = get_crossposts(start_epoch, subreddits)
+df = get_crossposts(start_epoch, subreddits, 'test')
+
+df3 = aggregate(df)
+
 
 df3.to_csv('../../Files/networktest.csv')
