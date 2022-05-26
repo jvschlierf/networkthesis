3c4047618d31cd662677ebf238dffe7ab9f4b4c4
jvschlierf <67417254+jvschlierf@users.noreply.github.com>
2022-05-26 20:06:55 +0200

    tested for larger samples

--- Preprocessing/api_find_subs.py
+++ Preprocessing/api_find_subs.py
@@ -4,9 +4,21 @@ import time
 import datetime as dt 
 import pandas as pd
 import pickle 
+import argparse as arg
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+# parser = arg.ArgumentParser()
+# parser.add_argument()
+
 
 api = PushshiftAPI()
-start_epoch = int(dt.datetime(2021, 2, 1).timestamp())
+start_epoch = int(dt.datetime(2021, 1, 1).timestamp())
+end_epoch = int(dt.datetime(2021, 2, 1).timestamp())
 
 # OPEN
 # - Args to influence the following:
@@ -17,42 +29,47 @@ start_epoch = int(dt.datetime(2021, 2, 1).timestamp())
 
 subreddits = ['DebateVaccines', 'CovidVaccinated', 'Vaccine', 'Coronavirus', 'LockdownSkepticism', 'HermanCainAward', 'NoNewNormal']
 
-def get_crossposts_from(start_epoch, subreddits, outfile): # pulls submissions, then identifies where the posts are crossposted from
-    # OPEN:
-    # - Varying time depth
-    # - Depth of subreddit -> function to call get_crossposts_from
-    # - More than 600 -> get id of last post, set as 'after_id' value
-
-
+def get_crossposts_from(start_epoch, end_epoch, subreddits, outfile, limit=600, repeat=1): # pulls submissions, then identifies where the posts are crossposted from
     api = PushshiftAPI()
     results = []
-    df = pd.DataFrame(columns= ['subreddit', 'subreddit_id', 'subreddit_subscibers' 
-                'crosspost_parent', 'crosspost_parent_list'])
+    df = pd.DataFrame(columns= ['subreddit', 'subreddit_id', 'subreddit_subscribers', 
+                'crosspost_parent', 'crosspost_parent_list', 'created_utc', 'author'])
 
-    for i in subreddits: #for every subreddit, we pull posts given the set criteria
-        gen = api.search_submissions(
-            after = start_epoch,
-            subreddit = i,
-            filter=['subreddit', 'subreddit_id', 'subreddit_subscribers',
-                'crosspost_parent', 'crosspost_parent_list', 'author'],
-            limit = 600) #limit to avoid going over the rate limit.
+    for j in range(repeat): #for every subreddit, we pull posts given the set criteria
 
-        results = list(gen)
-        print(f'subreddit: {i}, number of results: {len(results)}')
-        temp = pd.DataFrame([thing.d_ for thing in results])
-        time.sleep(4) # wait to avoid going over the rate limit. Set higher if limit is increased.
+        for i in subreddits:
+            gen = api.search_submissions(
+                after = start_epoch,
+                before = end_epoch,
+                subreddit = i,
+                filter=['subreddit', 'subreddit_id', 'subreddit_subscribers',
+                    'crosspost_parent', 'crosspost_parent_list', 'created_utc', 'author'],
+                limit = limit) #limit to avoid going over the rate limit.
 
-        df = pd.concat([df, temp]) # Add results for one subreddit to the dataframe for all
+            results = list(gen)
+            print(f'subreddit: {i}, number of results: {len(results)}')
+            temp = pd.DataFrame([thing.d_ for thing in results])
+            time.sleep(2) # wait to avoid going over the rate limit. Set higher if limit is increased.
+            df = pd.concat([df, temp]) # Add results for one subreddit to the dataframe for all
+            start_epoch = df['created_utc'].tail(1).values[0] # Take last post as start for next repeat
+            end_epoch = start_epoch + 2678400 #ensuring that end time is one month after start time
 
+    df2 = df[df['crosspost_parent_list'].notna()].reset_index()
+    df2 = df2[df2['crosspost_parent_list'].str.len() != 0] #sometimes, this field contains an empty list
 
-    df2 = df[df['crosspost_parent'].notna()].reset_index()
+    df2['t'] = df2['crosspost_parent_list'].apply(lambda x: dict(x[0]))
+    df2['crosspost_from'] = df2['t'].apply(lambda x: x['subreddit'])
+    df2['crosspost_from_id'] = df2['t'].apply(lambda x: x['subreddit_id'])
+    df2['crosspost_from_subs'] = df2['t'].apply(lambda x: x['subreddit_subscribers'] )
+    df2['crosspost_from_num'] = df2['t'].apply(lambda x: x['num_crossposts'] )
 
-    for i in range(len(df2)): #pull crosspost_from information from field 'crosspost_parent_list' (which is in a json format)
-        # Using the field has the advantage that we can deal with deleted posts which we could not find using the reddit API.
-        t = dict(df2.loc[i,'crosspost_parent_list'][0])
-        df2.loc[i,'crosspost_from'] = t['subreddit']
-        df2.loc[i,'crosspost_from_id'] = t['subreddit_id']
-        df2.loc[i,'crosspost_from_subs'] = t['subreddit_subscribers'] 
+    # for i, r in df2.iterrows(): # Pull crosspost_from information from field 'crosspost_parent_list' (which is in a json format)
+    #     # Using the field has the advantage that we can deal with deleted posts which we could not find using the reddit API.
+    #     t = dict(r['crosspost_parent_list'][0])
+    #     r['crosspost_from'] = t['subreddit']
+    #     r['crosspost_from_id'] = t['subreddit_id']
+    #     r['crosspost_from_subs'] = t['subreddit_subscribers'] 
+    #     r['crosspost_from_num'] = t['num_crossposts'] 
 
     df2.to_pickle(f'../../Files/{outfile}.pickle') # save file to avoid straining the API
     return df2
@@ -66,7 +83,7 @@ def get_crossposts_to():
 
 
 def aggregate(df): #aggregate over the subreddits so that we get a list with subreddit - crosspost from and counts
-    df3 = df.groupby(['subreddit','subreddit_id','crosspost_from', 'crosspost_from_id']).agg({'subreddit_subscribers': 'mean', 'crosspost_from_subs': 'mean' , 'author' : 'count'}).reset_index()
+    df3 = df.groupby(['subreddit','subreddit_id','crosspost_from', 'crosspost_from_id']).agg({'subreddit_subscribers': 'mean', 'crosspost_from_subs': 'mean' , 'author' : 'count', 'crosspost_from_num': 'sum'}).reset_index()
     df3.rename(columns = {'author':'count',}, inplace = True)
     df3['crosspost_from_subs'] = df3['crosspost_from_subs'].astype(int)
 
@@ -74,21 +91,25 @@ def aggregate(df): #aggregate over the subreddits so that we get a list with sub
 
 def importance(df): #calculate the importance (similar to tf-idf)
     # OPEN
-    # - Remerge total counts of subreddits to og df, then calculate tf-idf 
     # - make work with Crosspost from
     imp = df.groupby(['crosspost_from']).agg({'subreddit': 'count', 'count': 'sum'})
-    imp.rename(columns ={'count': 'total'}) 
+    imp = imp.rename(columns ={'count': 'total'}) 
     imp.drop(['subreddit'], axis=1, inplace=True)
-    df2 = df.join(imp, on='crosspost_from', how='left')
+    df = df
+    df2 = df.merge(imp, on='crosspost_from', how='left')
     return df2
 
 
-# df = get_crossposts_from(start_epoch, subreddits, 'test')
 
-# df3 = aggregate(df)
-# df3.to_csv('../../Files/networktest.csv')
+def depth():
+    pass
+    # get list of subreddits_from to then call crosspost_from on them as well
 
-df = pd.read_csv('../../Files/networktest.csv')
-df.rename(columns = {'author':'count',}, inplace = True)
-df2 = importance(df)
-df2.to_csv('../../Files/sfiaf.csv')
+df = get_crossposts_from(start_epoch, end_epoch, subreddits, '2021-01-02', repeat=3)
+
+# df = pd.read_pickle('../../Files/2021-01-02.pickle')
+df2 = aggregate(df)
+
+# df.rename(columns = {'author':'count',}, inplace = True)
+df3 = importance(df2)
+df3.to_csv('../../Files/tfidf.csv')
