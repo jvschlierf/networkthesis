33b792f5933b2a90960f5cefd2a20701f1c25add
Jakob Schlierf <jvschlierf@gmail.com>
2022-07-20 13:41:18 +0200

    added merge script

--- Preprocessing/merge_distr_subs.py
+++ Preprocessing/merge_distr_subs.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd 
 import argparse as arg
 import sys
+import datetime
+import csv
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -13,7 +15,7 @@ parser.add_argument('ptype', type=str, help='either Sumbissions or Comments')
 
 args = parser.parse_args(sys.argv[1:])
 
-
+print(args.ptype)
 # get files in directory and create list of files
 def get_files(dir):
     files = []
@@ -23,10 +25,10 @@ def get_files(dir):
     return files.sort()
 
 
-def identify_missing_files(files):
+def identify_missing_files(dir, files):
     missing_files = []
     for file in files:
-        if not os.path.exists(os.path.join(dir, file)):
+        if not os.path.exists(os.path.join(dir, file)) or os.path.getsize(os.path.join(dir, file)) <= 459:
             missing_files.append(file)
     return missing_files
 
@@ -39,7 +41,7 @@ def splittimeframe(subreddit, start, end, split): # splits the time into a serie
             e = int((start + (i + 1) * step) - 1)
             if i == split - 1:
                     e += 86400
-            split_list.append([subreddit, s])
+            split_list.append(f'{subreddit}-{s}.pickle')
 
     return split_list
 
@@ -49,3 +51,28 @@ def find_existing_pulls(type, subreddits): #remove existing pulls from subreddit
         done[done.index(i)] = i[:-7]
     res = [i for i in subreddits if i not in done]
     return res
+
+
+subreddits_not_completed = find_existing_pulls(args.ptype, args.subreddits)
+
+start = int(datetime.datetime(2020, 3, 1).timestamp())
+end = int(datetime.datetime(2022, 3, 31).timestamp())
+split = 24
+missing_parts = []
+for subreddit in subreddits_not_completed:
+    
+    subreddit_parts = splittimeframe(subreddit, start, end, split)
+    missing = identify_missing_files(f'../../Files/{args.ptype}/temp/', subreddit_parts)
+    for m in missing:
+        missing_parts.append(m)
+
+
+file = open(f'../../Files/{args.ptype}/temp/missing.csv', "w")
+writer = csv.writer(file, delimiter = "\n")
+for list_ in missing_parts:
+     writer.writerow([list_])
+file.close()
+
+
+
+
---
+++ Preprocessing/remaing_pull_subs.py
@@ -0,0 +1,142 @@
+import pmaw
+import os
+import pandas as pd
+import logging
+import argparse as arg
+from multiprocessing import Pool
+import sys
+import datetime
+import csv
+import time
+import random
+from itertools import chain
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+processes = 6
+
+
+parser = arg.ArgumentParser()
+parser.add_argument('subreddits', type=str, help='csv list of subreddits to pull')
+parser.add_argument('ptype', type=str, help='type of pull to do')
+
+
+args = parser.parse_args(sys.argv[1:])
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename=f'../../Files/logs/distr_pull_subs{datetime.datetime.today()}.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S'
+)
+
+def pullSubreddit(subreddit,start, end, ptype): # Pulls a subreddit from reddit and saves it to a file
+    api = pmaw.PushshiftAPI(
+    num_workers=10,
+    rate_limit=100,
+    jitter='full'
+    )
+    
+    if ptype == 'Submissions':
+        results = api.search_submissions(
+            subreddit=subreddit, 
+            after=start, 
+            before=end,
+            mem_safe=True,
+            filter = ('author', 
+                'title',
+                'created_utc',
+                'selftext',
+                'url',
+                'id',
+                'score',
+                'num_comments',
+                'subreddit',
+                'permalink')
+            )
+    elif ptype == 'Comments':
+        results = api.search_comments(
+        subreddit=subreddit,
+        after=start,
+        before=end,
+        mem_safe=True,
+        filter = ('author',
+            'body',
+            'created_utc',
+            'id',
+            'score',
+            'permalink',
+            'subreddit',
+            'parent_id')
+        )   
+
+    temp = pd.DataFrame([thing for thing in results])
+    logging.info(f'Pulled subreddit {subreddit} number of {ptype}: {len(temp)}')
+    return temp
+    
+
+
+
+def find_existing_pulls(type, subreddits): #remove existing pulls from subreddits list
+    done = os.listdir(f'../../Files/{type}/')
+    for i in done:
+        done[done.index(i)] = i[:-7]
+    res = [i for i in subreddits if i not in done]
+    return res
+
+def splittimeframe(subreddit, start, end, split): # splits the time into a series of 
+    split_list = []
+    step = (end - start) / split
+    for i in range(split):
+            s = int(start + i * step)
+            e = int((start + (i + 1) * step) - 1)
+            if i == split - 1:
+                    e += 86400
+            split_list.append([subreddit, s, e])
+
+    return split_list
+
+def main(subreddit): 
+    ptype = 'Submissions'
+    t = random.randint(0, 600)
+    logging.info(f'Pulling subreddit: {subreddit}, but first sleeping for {t}')
+    time.sleep(t)
+    start = int(datetime.datetime(2020, 3, 1).timestamp())
+    end = int(datetime.datetime(2022, 3, 31).timestamp())
+    split = 24
+    step = (end - start) / split
+    temp = pullSubreddit(subreddit[0], subreddit[1], subreddit[1]+step, ptype)
+    if len(temp) == 0:
+        logging.warning(f'Failed to pull Subreddit {subreddit} with {datetime.datetime.fromtimestamp(begin).strftime("%m/%d/%Y, %H:%M:%S")}')
+        time.sleep(10)
+    try:
+        logging.info(f'pulled Subreddit {subreddit} with {ptype} {len(temp)} with begin {datetime.datetime.fromtimestamp(begin).strftime("%m/%d/%Y, %H:%M:%S")}')
+        temp.to_pickle(f'../../Files/{ptype}/temp/{subreddit}-{begin}.pickle')
+    except:
+        logging.info(f'failed to save Subreddit {subreddit} with {ptype} {len(temp )} with begin {datetime.datetime.fromtimestamp(begin).strftime("%m/%d/%Y, %H:%M:%S")} ')
+        pass
+
+
+with open(args.subreddits, newline='') as f:
+    reader = csv.reader(f)
+    subreddits = list(reader)
+
+subreddits = list(chain.from_iterable(subreddits))
+
+subs_start = []
+for subreddit in subreddits:
+    subs_start.append([subreddit[:-18], subreddit[-17:-7]])
+
+if __name__ == '__main__':
+    logging.info('start')
+    subreddits = find_existing_pulls(args.ptype, subreddits)
+    logging.info(f'pulling submissions for {len(subreddits)} subreddits')
+    p1 = Pool(processes, maxtasksperchild=2)
+    
+    p1.map(main, subreddits)
+    p1.close()
+   
+    logging.info('finished')
