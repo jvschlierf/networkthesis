0918730f65147a314e3dac35b4da0ed766c1c6b5
Jakob Schlierf <jvschlierf@gmail.com>
2022-07-26 22:31:39 +0200

    trying to speed up pulling

--- classifier/nn/main.py
+++ classifier/nn/main.py
@@ -3,3 +3,15 @@ File to control text classification models as part of Analysis of Pro- & Anti- V
 """
 import logging
 import os
+import torch
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", model_args={"num_labels": 3},  )
+
+inputs = tokenizer("Hello, my dog is ugly", return_tensors="pt")
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+predicted_class_id = logits.argmax().item()
+print(model.config.id2label[predicted_class_id])
