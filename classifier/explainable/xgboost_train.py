54209462422e05b31e54df9d8698379855c1e55e
JAKOB SCHLIERF (jakob.schlierf@studbocconi.it) <3147567@dsba.sm.unibocconi.it>
2022-08-10 12:18:36 +0200

    Pro tip: don't name your files like modules

--- classifier/explainable/xgboost_train.py
+++ classifier/explainable/xgboost_train.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 import numpy as np
 from sklearn.model_selection import GridSearchCV
-import xgboost as xgb
+import classifier.explainable.xgboost_train as xgb
 from joblib import dump
 
 target = 'label'
