e1c0494e21b548864b169cc0f31b2a98d8e07591
JAKOB SCHLIERF (jakob.schlierf@studbocconi.it) <3147567@dsba.sm.unibocconi.it>
2022-08-19 20:49:05 +0200

    predict_BERT

--- classifier/nn/bert_base_predict.py
+++ classifier/nn/bert_base_predict.py
@@ -1,31 +1,42 @@
 
 import os
 import torch
-from transformers import Trainer, TrainingArguments, AutoTokenizer, AutoModel, pipeline, AutoModelForSequenceClassification
+from transformers import  AutoTokenizer, AutoModelForSequenceClassification
 import numpy as np
 import pandas as pd
 from transformers import TextClassificationPipeline
+from tqdm import tqdm
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-test = pd.read_pickle('../../../Files/Submissions/train/test_split_submission.pickle')
-test['text'] = test['cleanTitle']
-test = test[['text']]
 
-test = test.values.tolist()
 
 model_name = 'bert-base-cased'
 tokenizer = AutoTokenizer.from_pretrained(model_name, padding="max_length", truncation=True)
-model = AutoModelForSequenceClassification.from_pretrained('../../../Files/models/bert_base_cased_model/fully_trained/checkpoint-3237/')
-classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+model = AutoModelForSequenceClassification.from_pretrained('../../../Files/models/bert_base_cased_model/fully_trained_5/checkpoint-2158/')
 
-from tqdm import tqdm
-predictions = []
-for i in tqdm(range(len(test))):
-    predictions.append(classifier(test[i]))
+classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, device=1)
 
+files = os.listdir('../../../Files/Submissions/score/')
 
-df = pd.DataFrame(predictions)
-df.to_pickle('../../../Files/models/bert_base_cased_predictions.pickle')
+# remove any files that are not in the .pickle type 
+files = [f for f in files if f.endswith('.pickle')]
+
+print(f"setup completed, scoring {len(files)} subreddits")
+
+for file in tqdm(files):
+    test = pd.read_pickle(f'../../../Files/Submissions/{file}')
+    testlist = []
+    for i,j in test.iterrows():
+        testlist.append(j['cleanTitle'])
+    results = classifier(testlist[:100], top_k=2)
+
+    for i, j in test.iterrows():
+        test.at[i, 'pred_1'] = np.int64(results[i][0]['label'][-1])
+        test.at[i, 'conf_1'] = results[i][0]['score']
+        test.at[i, 'pred_2'] = np.int64(results[i][1]['label'][-1])
+        test.at[i, 'conf_2'] = results[i][1]['score']
+    
+    test.to_pickle(f'../../../Files/Submissions/score/{file}')
