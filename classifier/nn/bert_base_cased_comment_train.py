3f4868bb8c0a53a0653a1323edf90a5aa3facf2d
Jakob Schlierf <jvschlierf@gmail.com>
2022-08-25 00:36:30 +0200

    adding eval steps

--- classifier/nn/bert_base_cased_comment_train.py
+++ classifier/nn/bert_base_cased_comment_train.py
@@ -62,7 +62,8 @@ training_args = TrainingArguments(
     evaluation_strategy='epoch',
     logging_dir='../../../Files/logs/', 
     save_strategy = "epoch",
-    save_steps=10_000, save_total_limit=4, )
+    save_steps=10_000, save_total_limit=4,
+    eval_accumulation_steps=16, )
 
 
 
