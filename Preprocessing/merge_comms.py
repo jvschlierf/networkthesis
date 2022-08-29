613c47f7d2374741e79fc6ec75d8bf9710e6d46d
Jakob Schlierf <jvschlierf@gmail.com>
2022-08-29 11:13:41 +0200

    corrected directory

--- Preprocessing/merge_comms.py
+++ Preprocessing/merge_comms.py
@@ -69,7 +69,7 @@ for comment in comments_open:
     if check == len(timestamps)- 1:
         print(f'passed length check for {comment}')
         submission = pd.read_pickle(f'../../Files/Submissions/score/{comment}.pickle')
-        df1 = pd.read_pickle(os.path.join('../../FilesComments/temp/', comment_files[0]))
+        df1 = pd.read_pickle(os.path.join('../../Files/Comments/temp/', comment_files[0]))
         if math.isclose(submission.created_utc.min(), df1.created_utc.min(), rel_tol=.0000023): # we require the first comment to be created within an hour of the first submission
             print(f'passed start check for {comment}')
             df2 = pd.read_pickle(os.path.join('../../FilesComments/temp/', comment_files[-1]))
