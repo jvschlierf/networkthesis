b5acf7a0cb5f8a42659ac376a93b70abf66c54ca
Jakob Schlierf <jvschlierf@gmail.com>
2022-08-30 18:04:44 +0200

    batch it

--- classifier/nn/Roberta_predict.py
+++ classifier/nn/Roberta_predict.py
@@ -28,7 +28,7 @@ model_name = "cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(model_name, max_length=500, padding=True, truncation=True,  add_special_tokens=True)
 model = AutoModelForSequenceClassification.from_pretrained(os.path.join('../../../Files/models/', args.model_dir))
 
-classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, truncation=True,  max_length=500, device=1)
+classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, truncation=True,  max_length=500, device=1, batch_size=64)
 
 try: 
     files = os.listdir(os.path.join('../../../Files/', args.dir_path))
