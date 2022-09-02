f7a268bd6a563a89d49292a9810ae7406f9e8f40
Jakob Schlierf <jvschlierf@gmail.com>
2022-09-02 18:13:44 +0200

    run with messages so Jakob sees progress

--- analysis/topic_model.py
+++ analysis/topic_model.py
@@ -7,19 +7,25 @@ from tqdm import tqdm
 import time 
 import multiprocessing 
 
+columns=['cleanText']
+
 df0 = pd.read_parquet('../../Files/Submissions/score/done/Anti_vac.parquet')
+print('loaded first file')
 df1 = pd.read_parquet('../../Files/Submissions/score/done/Neutr.parquet')
+print('loaded second file')
 df2 = pd.read_parquet('../../Files/Submissions/score/done/Pro_vac.parquet')
+print('loaded third file, merging')
 
 
 df = pd.concat([df0, df1, df2], ignore_index=True)
-
+print('merged')
 instances = df['cleanText'].to_list()
-
+print('got instances')
 
 phrases = Phrases(instances, min_count=5, threshold=1)
+print('got phrases')
 instances_colloc = phrases[instances]
-
+print('got instance collocs')
 dictionary = Dictionary(instances_colloc)
 # get rid of words that are too rare or too frequent
 dictionary.filter_extremes(no_below=50, no_above=0.3)
@@ -39,7 +45,7 @@ coherence_values = []
 
 
 
-for num_topics in range(4, 20):
+for num_topics in tqdm(range(4, 20)):
     model = LdaMulticore(corpus=model_corpus, 
                          id2word=dictionary, 
                          num_topics=num_topics, random_state=42)
