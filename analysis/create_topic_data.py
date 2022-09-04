2b95d27cbd72a7fe7e4ac1ef7538efe6945e371c
Jakob Schlierf <jvschlierf@gmail.com>
2022-09-04 14:10:04 +0200

    topic data comms

--- analysis/create_topic_data.py
+++ analysis/create_topic_data.py
@@ -14,18 +14,18 @@ args = parser.parse_args(sys.argv[1:])
 files = os.listdir(os.path.join('../../Files/', args.dir_path))
 files = [file for file in files if file.endswith('.pickle')]
 
-Pro_Vac = pd.DataFrame(columns=['cleanText', 'score', 'subreddit', 'num_comments', 'pred_1',])
-Ant_Vac = pd.DataFrame(columns=['cleanText', 'score', 'subreddit', 'num_comments', 'pred_1'])
-Neutr = pd.DataFrame(columns=['cleanText', 'score', 'subreddit', 'num_comments', 'pred_1'])
+Pro_Vac = pd.DataFrame(columns=['cleanBody', 'score', 'subreddit',  'pred_1',])
+Ant_Vac = pd.DataFrame(columns=['cleanBody', 'score', 'subreddit',  'pred_1'])
+Neutr = pd.DataFrame(columns=['cleanBody', 'score', 'subreddit', 'pred_1'])
 
 for file in tqdm(files):
     df = pd.read_pickle(os.path.join('../../Files', args.dir_path, file))
 
-    df = df[['cleanText', 'score', 'subreddit', 'num_comments', 'pred_1']]
-    df['cleanText'] = df['cleanText'].str.replace(r'\[\s', '[', regex=True)
-    df['cleanText'] = df['cleanText'].str.replace(r'\s\]', ']', regex=True)
+    df = df[['cleanBody', 'score', 'subreddit', 'num_comments', 'pred_1']]
+    df['cleanBody'] = df['cleanBody'].str.replace(r'\[\s', '[', regex=True)
+    df['cleanBody'] = df['cleanBody'].str.replace(r'\s\]', ']', regex=True)
 
-    # df['cleanText'] = df['cleanText'].str.split(expand=False)
+    # df['cleanBody'] = df['cleanBody'].str.split(expand=False)
 
     df0 = df[df['pred_1'] == 0.0]
     df1 = df[df['pred_1'] == 1.0]
