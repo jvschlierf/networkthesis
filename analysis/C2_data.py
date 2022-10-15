7e33d7d6a106c7b66b328060afe6c59f6be43fa3
Jakob Schlierf <jvschlierf@gmail.com>
2022-10-15 17:04:49 +0100

    corrected misreading files

--- analysis/C2_data.py
+++ analysis/C2_data.py
@@ -2,22 +2,14 @@ import pandas as pd
 
 import os
 import sys
-import argparse as arg
 from tqdm import tqdm 
-from datetime import datetime
-
-parser = arg.ArgumentParser()
-parser.add_argument('dir_path', type=str, help='directory or path file to predict')
 
 
-args = parser.parse_args(sys.argv[1:])
-
-files = os.listdir(os.path.join('../../Files/', args.dir_path))
+files = os.listdir(os.path.join('../../Files/Submissions/score/'))
 files = [file for file in files if file.endswith('.pickle')]
 
 for file in tqdm(files):
-    temp = pd.read_pickle(os.path.join('../../Files', args.dir_path, file))
+    temp = pd.read_pickle(os.path.join('../../Files/Submissions/score/', file))
     temp = temp[temp['class_I'] == 1.0]
-    temp.to_pickle(os.path.join('../../Files', args.dir_path, '/done/', file, 'c_o.pickle'))
-
+    temp.to_pickle(os.path.join('../../Files/Submissions/score/done/', file))
 print('done writing to file')
