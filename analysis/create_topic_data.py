904d9e7936d01cb342fdc58d3aa3aad4282d7679
Jakob Schlierf <jvschlierf@gmail.com>
2022-10-23 16:10:46 +0200

    please work

--- analysis/create_topic_data.py
+++ analysis/create_topic_data.py
@@ -21,9 +21,9 @@ Neutr = pd.DataFrame(columns=['cleanText', 'score', 'subreddit', 'class_II'])
 
 for file in tqdm(files):
     df = pd.read_pickle(os.path.join('../../Files', args.dir_path, file))
-
+    print(len(df))
     df = df[df['class_I'] == 1.0]
-
+    print(len(df))
     df = df[['cleanText', 'score', 'subreddit', 'class_II']]
     df['cleanText'] = df['cleanText'].str.replace(r'\[\s', '[', regex=True)
     df['cleanText'] = df['cleanText'].str.replace(r'\s\]', ']', regex=True)
