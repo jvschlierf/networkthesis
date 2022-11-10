154c619987644ee63876ee35501f26e25d79de07
Jakob Schlierf <jvschlierf@gmail.com>
2022-11-10 23:28:09 +0100

    create Data for Duilio

--- analysis/create_topic_data.py
+++ analysis/create_topic_data.py
@@ -15,30 +15,30 @@ files = os.listdir(os.path.join('../../Files/', args.dir_path))
 files = [file for file in files if file.endswith('.pickle')]
 files = [file for file in files if file.startswith('d_')]
 
-Pro_Vac = pd.DataFrame(columns=['cleanText', 'score', 'subreddit',  'created_utc', 'class_II',])
-Ant_Vac = pd.DataFrame(columns=['cleanText', 'score', 'subreddit',  'created_utc',  'class_II'])
-Neutr = pd.DataFrame(columns=['cleanText', 'score', 'subreddit', 'created_utc',  'class_II'])
+Pro_Vac = pd.DataFrame(columns=['selftext', 'title', 'score', 'id', 'author', 'subreddit',  'created_utc', 'class_II', 'conf_II'])
+# Ant_Vac = pd.DataFrame(columns=['cleanText', 'score', 'subreddit',  'created_utc',  'class_II'])
+# Neutr = pd.DataFrame(columns=['cleanText', 'score', 'subreddit', 'created_utc',  'class_II'])
 
 for file in tqdm(files):
     df = pd.read_pickle(os.path.join('../../Files', args.dir_path, file))
     print(len(df))
     df = df[df['class_I'] == 1.0]
     print(len(df))
-    df = df[['cleanText', 'score', 'subreddit', 'created_utc', 'class_II']]
-    df['cleanText'] = df['cleanText'].str.replace(r'\[\s', '[', regex=True)
-    df['cleanText'] = df['cleanText'].str.replace(r'\s\]', ']', regex=True)
+    df = df[['selftext', 'title', 'score', 'id', 'author', 'subreddit',  'created_utc', 'class_II', 'conf_II']]
+    # df['cleanText'] = df['cleanText'].str.replace(r'\[\s', '[', regex=True)
+    # df['cleanText'] = df['cleanText'].str.replace(r'\s\]', ']', regex=True)
 
-    # df['cleanBody'] = df['cleanBody'].str.split(expand=False)
+    # # df['cleanBody'] = df['cleanBody'].str.split(expand=False)
 
-    df0 = df[df['class_II'] == 0.0]
-    df1 = df[df['class_II'] == 1.0] 
-    df2 = df[df['class_II'] == 2.0]
+    # df0 = df[df['class_II'] == 0.0]
+    # df1 = df[df['class_II'] == 1.0] 
+    # df2 = df[df['class_II'] == 2.0]
 
-    Ant_Vac = pd.concat([Ant_Vac, df0], ignore_index=True)
-    Neutr = pd.concat([Neutr, df1], ignore_index=True)
-    Pro_Vac = pd.concat([Pro_Vac, df2], ignore_index=True)
+    # Ant_Vac = pd.concat([Ant_Vac, df0], ignore_index=True)
+    # Neutr = pd.concat([Neutr, df1], ignore_index=True)
+    Pro_Vac = pd.concat([Pro_Vac, df], ignore_index=True)
 
 print('done splitting, writing to file')
-Ant_Vac.to_parquet(os.path.join('../../Files/', args.dir_path, 'Anti_vacc_d.parquet'))
-Neutr.to_parquet(os.path.join('../../Files/', args.dir_path, 'Neutr_vacc_d.parquet'))
-Pro_Vac.to_parquet(os.path.join('../../Files', args.dir_path, 'Pro_vacc_d.parquet'))
+# Ant_Vac.to_parquet(os.path.join('../../Files/', args.dir_path, 'Anti_vacc_d.parquet'))
+# Neutr.to_parquet(os.path.join('../../Files/', args.dir_path, 'Neutr_vacc_d.parquet'))
+Pro_Vac.to_pickle(os.path.join('../../Files', args.dir_path, 'ClassifierII_data.pickle'))
